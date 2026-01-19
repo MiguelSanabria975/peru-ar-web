054b7dd20b3354741ab5c41522b41959895344ee
Miguel <miguelsanabria975@gmail.com>
2026-01-19 13:00:32 -0500

    updtade app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactHashRouter } from '@ionic/react-router';
-
+import React from 'react'
+import { Redirect, Route } from 'react-router-dom'
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react'
+import { IonReactHashRouter } from '@ionic/react-router'
 
 import Home from './pages/Home'
 import Map from './pages/Map'
 import Passport from './pages/Passport'
+import Escaner from './pages/Escaner'
+import Perfil from './pages/Perfil'
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
-/* Theme variables */
-import './theme/variables.css';
-import Escaner from './pages/Escaner';
-import Perfil from './pages/Perfil';
-
-setupIonicReact();
+setupIonicReact()
 
 const App: React.FC = () => (
   <IonApp>
@@ -38,14 +18,12 @@ const App: React.FC = () => (
         <Route exact path="/home" component={Home} />
         <Route exact path="/map" component={Map} />
         <Route exact path="/passport" component={Passport} />
-        <Route exact path="/Escaner" component={Escaner} />
-        <Route exact path="/Perfil" component={Perfil} />
+        <Route exact path="/escaner" component={Escaner} />
+        <Route exact path="/perfil" component={Perfil} />
         <Redirect exact from="/" to="/home" />
       </IonRouterOutlet>
     </IonReactHashRouter>
   </IonApp>
-);
+)
 
-export default App;
-
-
+export default App
