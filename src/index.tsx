320392dae6a8502558b1047b4d48958c74fa3b73
Miguel <miguelsanabria975@gmail.com>
2026-01-19 12:55:26 -0500

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,37 @@
-  import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-const container = document.getElementById('root');
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const root = createRoot(container!);
+/*  CSS CORE DE IONIC (CRÍTICO) */
+import '@ionic/react/css/core.css'
+
+/*  CSS ESTRUCTURAL (CRÍTICO) */
+import '@ionic/react/css/normalize.css'
+import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
+
+/* CSS UTILITARIOS */
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
+
+/* Variables de tema */
+import './theme/variables.css'
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+import reportWebVitals from './reportWebVitals'
+
+const container = document.getElementById('root')
+const root = createRoot(container!)
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorkerRegistration.unregister()
+reportWebVitals()
