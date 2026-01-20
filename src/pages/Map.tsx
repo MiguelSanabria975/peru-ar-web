831cf5f02a502d617d83e81f19f4e464006d58a1
Miguel <miguelsanabria975@gmail.com>
2026-01-20 13:05:10 -0500

    updated

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
   IonPage,
   IonHeader,
@@ -8,10 +8,9 @@ import {
   IonIcon,
   IonTitle,
   IonContent,
-  IonCard,
-  IonCardContent,
   IonText,
-  IonChip
+  IonChip,
+  IonModal
 } from '@ionic/react'
 
 import {
@@ -26,36 +25,48 @@ import {
 } from 'ionicons/icons'
 
 import { useIonRouter } from '@ionic/react'
-import { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from 'leaflet'
+
 import { BTCSite, btcSites } from '../data/btcSites'
 import { ChakanaIcon } from '../components/icons/IncaIcons'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import './Map.css'
+
+const CUSCO_CENTER: [number, number] = [-13.53195, -71.96746]
 
 const createSiteIcon = (site: BTCSite) =>
   L.divIcon({
     className: 'custom-site-marker',
     html: `
       <div class="marker-core ${site.visited ? 'visited' : 'new'}">
-        ★
+        <span class="marker-star">★</span>
+        <span class="marker-name">${site.name}</span>
       </div>
     `,
-    iconSize: [42, 42],
-    iconAnchor: [21, 42]
+    iconSize: [120, 42],
+    iconAnchor: [60, 42]
   })
 
-const CUSCO_CENTER: [number, number] = [-13.53195, -71.96746]
-
-const MapPage = () => {
+const MapPage: React.FC = () => {
   const router = useIonRouter()
-  const [selectedSite, setSelectedSite] = useState<any>(null)
+
+  const mapRef = useRef<L.Map | null>(null)
+
+  const [selectedSite, setSelectedSite] = useState<BTCSite | null>(null)
 
   const totalXP = btcSites
     .filter(s => s.visited)
     .reduce((acc, s) => acc + s.xpReward, 0)
+
+  useEffect(() => {
+  const timeout = setTimeout(() => {
+    mapRef.current?.invalidateSize()
+  }, 400)
+
+  return () => clearTimeout(timeout)
+}, [])
 
   return (
     <IonPage>
@@ -68,8 +79,6 @@ const MapPage = () => {
           </IonButtons>
 
           <IonTitle>Camino Sagrado</IonTitle>
-
-          
 
           <IonButtons slot="end">
             <IonButton>
@@ -98,110 +107,97 @@ const MapPage = () => {
           </IonChip>
         </div>
 
-       {/* MAPA FUNCIONAL */}
-<div className="map-wrapper">
-  <MapContainer
-    center={CUSCO_CENTER}
-    zoom={14}
-    scrollWheelZoom={false}
-    className="map-container"
-  >
-    <TileLayer
-      attribution="&copy; OpenStreetMap"
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+        {/* MAPA */}
+        <div className="map-wrapper">
+         <MapContainer
+          center={CUSCO_CENTER}
+          zoom={14}
+          scrollWheelZoom={false}
+          className="map-container"
+          ref={mapRef}
+        >
+            <TileLayer
+              attribution="&copy; OpenStreetMap"
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-    <Marker position={CUSCO_CENTER}>
-      <Popup>Tu ubicación actual</Popup>
-    </Marker>
+            {/* Usuario */}
+            <Marker position={CUSCO_CENTER}>
+              <Popup>Tu ubicación actual</Popup>
+            </Marker>
 
-    {btcSites.map(site => (
-  <Marker
-    key={site.id}
-    position={[site.lat, site.lng]}
-    icon={createSiteIcon(site)}
-    eventHandlers={{
-      click: () => setSelectedSite(site)
-    }}
-  >
-    <Popup>
-      <strong>{site.name}</strong>
-    </Popup>
+            {/* Sitios */}
+            {btcSites.map(site => (
+              <Marker
+                key={site.id}
+                position={[site.lat!, site.lng!]}
+                icon={createSiteIcon(site)}
+                eventHandlers={{
+                  click: () => setSelectedSite(site)
+                }}
+              />
+            ))}
+          </MapContainer>
 
-    {btcSites.map(site => (
-  <div
-    key={`${site.id}-label`}
-    className="site-label"
-    style={{
-      top: `${site.lat}%`,
-      left: `${site.lng}%`
-    }}
-  >
-    {site.name}
-  </div>
-))}
+          <div className="user-location-label">
+            <ChakanaIcon className="chakra-icon" />
+            <span>Estás en Cusco</span>
+          </div>
+        </div>
 
-  </Marker>
-))}
-  </MapContainer>
+        {/* MODAL */}
+        <IonModal
+          isOpen={!!selectedSite}
+          onDidDismiss={() => setSelectedSite(null)}
+          breakpoints={[0, 0.45]}
+          initialBreakpoint={0.45}
+        >
+          {selectedSite && (
+            <div className="site-modal">
+              <div className="site-header">
+                <IonIcon icon={star} className="site-icon" />
+                <span className="site-type">
+                  {selectedSite.type.toUpperCase()}
+                </span>
+              </div>
 
-  <div className="user-location-label">
-    <ChakanaIcon className="chakra-icon" />
-    <span>Estás en Cusco</span>
-  </div>
-</div>
+              <h2>{selectedSite.name}</h2>
+              <p>{selectedSite.description}</p>
 
-    {selectedSite && (
-  <div className="site-modal">
+              <div className="site-meta">
+                <IonChip>+{selectedSite.xpReward} XP</IonChip>
+                <IonChip>
+                  {selectedSite.visited ? 'Visitado' : 'Nuevo'}
+                </IonChip>
+              </div>
 
-    {/* ICONO + TIPO */}
-    <div className="site-header">
-      <IonIcon
-        icon={star}
-        className={`site-icon ${selectedSite.type}`}
-      />
-      <span className="site-type">
-        {selectedSite.type.toUpperCase()}
-      </span>
-    </div>
+              <IonButton expand="block" color="warning">
+                Explorar sitio
+              </IonButton>
 
-    <h2 className="site-title">{selectedSite.name}</h2>
+              <IonButton
+                expand="block"
+                fill="clear"
+                onClick={() => setSelectedSite(null)}
+              >
+                Cerrar
+              </IonButton>
+            </div>
+          )}
+        </IonModal>
 
-    <p className="site-description">
-      {selectedSite.description}
-    </p>
-
-    <div className="site-meta">
-      <IonChip className="xp-chip">
-        +{selectedSite.xpReward} XP
-      </IonChip>
-
-      <IonChip className={selectedSite.visited ? 'visited' : 'new'}>
-        {selectedSite.visited ? 'Visitado' : 'Nuevo'}
-      </IonChip>
-    </div>
-
-    <IonButton expand="block" color="warning">
-      Explorar sitio
-    </IonButton>
-
-    <IonButton
-      expand="block"
-      fill="clear"
-      onClick={() => setSelectedSite(null)}
-    >
-      Cerrar
-    </IonButton>
-  </div>
-)}
-
+        {/* ACCIONES */}
         {!selectedSite && (
           <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-50">
             <div className="flex gap-3 bg-black/80 px-4 py-2 rounded-full border border-white/20">
               <IonButton size="small" fill="clear">
                 <IonIcon icon={volumeHigh} />
               </IonButton>
-              <IonButton size="small" fill="clear" onClick={() => router.push('/scan')}>
+              <IonButton
+                size="small"
+                fill="clear"
+                onClick={() => router.push('/scan')}
+              >
                 <IonIcon icon={ticketOutline} />
               </IonButton>
             </div>
@@ -213,5 +209,3 @@ const MapPage = () => {
 }
 
 export default MapPage
-
-
